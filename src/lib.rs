0a77df82e2626ad27530269bdb61dea62e019d07
Andreas Maier <andreas.maier@durham.ac.uk>
2018-08-13 23:48:20 +0200

    added documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,73 @@
+//! This is a crate for handling Laurent series in a single variable
+//! about zero, i.e. expressions of the form
+//!
+//! \\[ s = \sum_{n=n_0}^N a_n x^n + O(x^{N+1}), \\]
+//!
+//! where \\( n_0 \\) and \\( N \\) are integers. Such series can be
+//! added, subtracted, multiplied, and divided. Some simple functions
+//! like powers of series, natural logarithms and exponentials are also
+//! implemented.
+//!
+//! The kinds of operations that can be performed depends on the data
+//! type of the variable and the coefficients. For example, we usually
+//! have to calculate the logarithm of both the leading coefficient and
+//! the expansion variable if we take the logarithm of a Laurent
+//! series. This crate is therefore most useful in combination with a
+//! crate providing at least basic symbolic math.
+//!
+//! # Examples
+//!
+//! ```rust
+//! use series::Series;
+//! use series::ops::{Ln,Exp};
+//!
+//! // Create a new series in x, starting at order x^2 with coefficients 1, 2, 3,
+//! // i.e. s = 1*x^2 + 2*x^3 + 3*x^4 + O(x^5).
+//! let s = Series::new("x", 2, vec!(1, 2, 3));
+//! println!("s = {}", s);
+//!
+//! // To show various kinds of operations we now switch to floating-point
+//! // coefficients
+//!
+//! // Now s = 1 - x + O(x^5).
+//! let s = Series::new("x", 0, vec!(1., -1., 0., 0.));
+//! // Expand 1/(1-x) up to x^4.
+//! let t = s.inverse();
+//! println!("1/(1-x) = {}", t);
+//!
+//! // Series can be added, subtracted, multiplied, divided.
+//! // We can either move the arguments or use references
+//! println!("s+t = {}", &s + &t);
+//! println!("s-t = {}", &s - &t);
+//! println!("s*t = {}", &s * &t);
+//! println!("s/t = {}", s/t);
+//!
+//! // More advanced operations in general require the variable type to be
+//! // convertible to the coefficient type by implementing the From trait.
+//! // In the examples shown here, this conversion is actually not required,
+//! // so we can get away with a dummy implementation.
+//! #[derive(Debug,Clone,PartialEq)]
+//! struct Variable<'a>(&'a str);
+//!
+//! impl<'a> From<Variable<'a>> for f64 {
+//!     fn from(_s: Variable<'a>) -> f64 {
+//!         panic!("Can't convert variable into f64")
+//!     }
+//! }
+//!
+//! impl<'a> std::fmt::Display for Variable<'a> {
+//!     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+//!         self.0.fmt(f)
+//!     }
+//! }
+//!
+//! // Now we can calculate logarithms, exponentials, and powers:
+//! let s = Series::new(Variable("x"), 0, vec!(1., -3., 5.));
+//! println!("exp(s) = {}", s.clone().exp());
+//! println!("ln(s) = {}", s.clone().ln());
+//! let t = s.clone();
+//! println!("s^s = {}", s.pow(t));
+//! ```
 use std::fmt;
 use std::ops::{Add,AddAssign,Sub,SubAssign,Mul,MulAssign,Div,DivAssign,Neg};
 use std::convert::From;
@@ -9,7 +79,7 @@ use self::ops::{Ln,Exp};
 type Pow = i32;
 
 #[derive(PartialEq,Debug,Clone)]
-struct Series<Var, Coeff> {
+pub struct Series<Var, Coeff> {
     var: Var,
     min_pow: Pow,
     coeffs: Vec<Coeff>,
