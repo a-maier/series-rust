c00de75916fb047a101acd1308a66a0eda423d71
Andreas Maier <andreas.martin.maier@desy.de>
2024-09-02 10:25:54 +0200

    fix another lifetime

--- src/poly_slice_in.rs
+++ src/poly_slice_in.rs
@@ -183,7 +183,7 @@ impl<'a, Var, C: Coeff> PolynomialSliceIn<'a, Var, C> {
     /// let s = series::PolynomialIn::new("x", -1, vec!(1,2,3));
     /// assert_eq!(s.as_slice(..).var(), &"x");
     /// ```
-    pub fn var(&self) -> &Var {
+    pub fn var(&self) -> &'a Var {
         self.var
     }
 
