2f3d360076041211d099482703dc2db922a3c2f4
Andreas Maier <amaier@ifae.es>
2026-03-26 16:18:07 +0100

    document `var` macro

--- src/var.rs
+++ src/var.rs
@@ -16,6 +16,28 @@ impl<Var: Default + Display> Display for VarParseError<Var> {
     }
 }
 
+/// Define a compile-time variable
+///
+/// This is a convenience macro for defining variables for
+/// [Series](crate::Series) or [Polynomial](crate::Polynomial).
+/// Effectively, `var!(VarName)` defines a struct `VarName` together
+/// with a compile-time constant `VAR_NAME` and a string constant
+/// `VAR_NAME_STR == "var_name"`. [Display] and
+/// [FromStr](std::str::FromStr) are implemented matching the value of
+/// this string constant.
+///
+/// # Example
+/// ```
+/// # use series::{Polynomial, var};
+/// var!(X);
+/// let p: Polynomial<X, i32> = "x".parse().unwrap();
+/// assert_eq!(p.to_string(), "x");
+///
+/// // with limited visibility
+/// var!{pub(crate) Y};
+/// let p: Polynomial<Y, i32> = "y".parse().unwrap();
+/// assert_eq!(p.to_string(), "y");
+/// ```
 #[macro_export]
 macro_rules! var {
     ($var:ident) => {
