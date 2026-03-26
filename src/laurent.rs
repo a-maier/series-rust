b7534f70afaa8379ea7e785f6139115d653f3b1c
Andreas Maier <amaier@ifae.es>
2026-03-26 16:30:03 +0100

    add serde support for Laurent

--- src/laurent.rs
+++ src/laurent.rs
@@ -14,6 +14,7 @@ use crate::{
 };
 
 /// A Laurent polynomial or series in a single variable
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[derive(
     Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Display, From, IsVariant,
 )]
